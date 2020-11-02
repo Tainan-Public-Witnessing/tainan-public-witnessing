9105088dc352fffe32ac87e5db62f9c20fc8fd39
philliptsai <philliptsai@gmail.com>
2020-11-02 14:07:24 +0800

    app component: toolbar sidenav

--- src/app/_modules/angular-material.module.ts
+++ src/app/_modules/angular-material.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -9,7 +12,10 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class AngularMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AngularMaterialModule } from 'src/app/_modules/angular-material/angular-material.module';
+import { AngularMaterialModule } from 'src/app/_modules/angular-material.module';
 
 @NgModule({
   declarations: [
