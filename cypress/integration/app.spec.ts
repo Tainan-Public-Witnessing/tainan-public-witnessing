74a57d3a836fd8166290e6797af1537fe68355e2
philliptsai <philliptsai@gmail.com>
2021-07-14 20:40:42 +0800

    remove app.component element id

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -1,6 +1,25 @@
 describe('', () => {
-  it('should initially displayed well', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('should initially displayed well', () => {
     cy.get('span[cy="app-title"]').should('contain.text', 'Tainan Public Witnessing');
+  });
+
+  it('should toggle menu if menu button clicked', () => {
+    cy.get('app-menu').should('not.be.visible');
+
+    cy.get('button[cy="menu-button"]').click();
+    cy.get('app-menu').should('be.visible');
+
+    cy.get('button[cy="menu-button"]').click();
+    cy.get('app-menu').should('not.be.visible');
+  });
+
+  it('should change language if language button clicked', () => {
+    cy.get('button[cy="translate-button"]').click();
+    cy.get('button[cy="language-button-zh"]').click();
+    cy.get('span[cy="app-title"]').should('contain.text', '台南都市公眾見證');
   });
 });
