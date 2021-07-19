a4b34063079fcb2b6e865de9e926e0184758f8fe
philliptsai <philliptsai@gmail.com>
2021-07-19 16:45:56 +0800

    login test

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -19,7 +19,36 @@ describe('', () => {
 
   it('should change language if language button clicked', () => {
     cy.get('button[cy="translate-button"]').click();
-    cy.get('button[cy="language-button-zh"]').click();
+    cy.get('button[cy="language-buttons"]').contains('Chinese').click();
     cy.get('span[cy="app-title"]').should('contain.text', '台南都市公眾見證');
   });
+
+  it('should login successfully', () => {
+    cy.get('button[cy="menu-button"]').click();
+    cy.get('mat-list-option[cy="login-button"]').click();
+    cy.get('input[cy="username-input"]').type('admin');
+    cy.get('mat-option[cy="username-autocomplete-options"]').contains('administrator').click();
+    cy.get('input[cy="password-input"]').type('admin');
+    cy.get('button[cy="login-submit-button"]').click();
+    cy.get('span[cy="welcome-message"]').should('contain.text', 'Hi! administrator');
+  });
+
+  it('should display error message if username incorrect while login', () => {
+    cy.get('button[cy="menu-button"]').click();
+    cy.get('mat-list-option[cy="login-button"]').click();
+    cy.get('input[cy="username-input"]').type('wrong username');
+    cy.get('input[cy="password-input"]').type('admin');
+    cy.get('button[cy="login-submit-button"]').click();
+    cy.get('mat-error[cy="username-not-exist-error-message"]').should('contain.text', 'User name do not exist!');
+  });
+
+  it('should display error message if password incorrect while login', () => {
+    cy.get('button[cy="menu-button"]').click();
+    cy.get('mat-list-option[cy="login-button"]').click();
+    cy.get('input[cy="username-input"]').type('administrator');
+    cy.get('input[cy="password-input"]').type('wrong password');
+    cy.get('button[cy="login-submit-button"]').click();
+    cy.get('mat-error[cy="wrong-password-error-message"]').should('contain.text', 'Wrong password!');
+  });
+
 });
