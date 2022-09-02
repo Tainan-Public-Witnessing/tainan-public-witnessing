f81ba99734eb867333cd02323331963fccc0bd18
Jamie Zhang <asxasdfghjkl@gmail.com>
2022-09-02 21:04:30 +0800

    add list windowing and filter

--- src/app/_modules/angular-material.module.ts
+++ src/app/_modules/angular-material.module.ts
@@ -1,32 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { A11yModule } from '@angular/cdk/a11y';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { A11yModule } from "@angular/cdk/a11y";
+import { ScrollingModule } from "@angular/cdk/scrolling";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [
     MatAutocompleteModule,
     MatButtonModule,
@@ -46,7 +45,8 @@ import { A11yModule } from '@angular/cdk/a11y';
     MatToolbarModule,
     MatMomentDateModule,
     DragDropModule,
+    ScrollingModule,
     A11yModule,
-  ]
+  ],
 })
-export class AngularMaterialModule { }
+export class AngularMaterialModule {}
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,8 +2,11 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
-import { BehaviorSubject, Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { BehaviorSubject, combineLatest, Subject } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged, takeUntil
+} from "rxjs/operators";
 import { Mode } from "src/app/_enums/mode.enum";
 import { UserKey } from "src/app/_interfaces/user.interface";
 import { AuthorityService } from "src/app/_services/authority.service";
@@ -18,6 +21,8 @@ export class UsersComponent implements OnInit, OnDestroy {
   userPrimarykeys$ = new BehaviorSubject<UserKey[] | null>(null);
   unsubscribe$ = new Subject<void>();
 
+  filterValue$ = new BehaviorSubject<string>("");
+
   constructor(
     private authorityService: AuthorityService,
     private translateService: TranslateService,
@@ -27,10 +32,21 @@ export class UsersComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.usersService
-      .getUserKeys()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe(this.userPrimarykeys$);
+    combineLatest([
+      this.usersService.getUserKeys().pipe(takeUntil(this.unsubscribe$)),
+      this.filterValue$.pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        takeUntil(this.unsubscribe$)
+      ),
+    ]).subscribe(([users, filter]) => {
+      if (!users) return [];
+
+      const filterReg = new RegExp(filter, "i");
+      this.userPrimarykeys$.next(
+        users.filter((user) => filterReg.test(user.username))
+      );
+    });
   }
 
   ngOnDestroy(): void {
