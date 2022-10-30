6d71b56a0c93ff822c4fd3f05a17667f593b9e51
Huang Kuan Wei <xellosiris@gmail.com>
2022-10-30 16:21:52 +0800

    project id 修正

--- backend/backup.py
+++ backend/backup.py
@@ -7,7 +7,7 @@ def Backup():
     body = {
         "outputUriPrefix": outputUriPrefix,
     }
-    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+    project_id = "tainan-public-witnessing-v2211"
     database_name = f"projects/{project_id}/databases/(default)"
     service = build("firestore", "v1")
     service.projects().databases().exportDocuments(
