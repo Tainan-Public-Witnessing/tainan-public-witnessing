29bf3870f44e5bbaf9ce536992335dc5851dca24
Huang Kuan Wei <33425166+xellosiris@users.noreply.github.com>
2022-11-11 15:24:26 +0800

    fix member-input show inactive member (#57)

--- src/app/_elements/dialogs/crew-editor/member-input/member-input.component.ts
+++ src/app/_elements/dialogs/crew-editor/member-input/member-input.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -7,29 +14,36 @@ import { UserKey } from 'src/app/_interfaces/user.interface';
 @Component({
   selector: 'app-member-input',
   templateUrl: './member-input.component.html',
-  styleUrls: ['./member-input.component.scss']
+  styleUrls: ['./member-input.component.scss'],
 })
 export class MemberInputComponent implements OnInit, OnDestroy {
-
   @Input() control!: AbstractControl;
   @Input() userKeys!: UserKey[];
 
   autoCompleteUserKeys$ = new BehaviorSubject<string[]>([]);
   destroy$ = new Subject<void>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-
-    this.control.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(value => {
-      if (value === '') {
-        this.autoCompleteUserKeys$.next([]);
-      } else {
-        this.autoCompleteUserKeys$.next(
-          this.userKeys.filter(userKey => userKey.username.toLowerCase().includes(value.toLowerCase())).map(userKey => userKey.username)
-        );
-      }
-    })
+    this.control.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((value) => {
+        if (value === '') {
+          this.autoCompleteUserKeys$.next([]);
+        } else {
+          this.autoCompleteUserKeys$.next(
+            this.userKeys
+              .filter(
+                (userKey) =>
+                  userKey.username
+                    .toLowerCase()
+                    .includes(value.toLowerCase()) && userKey.activate
+              )
+              .map((userKey) => userKey.username)
+          );
+        }
+      });
   }
 
   ngOnDestroy(): void {
