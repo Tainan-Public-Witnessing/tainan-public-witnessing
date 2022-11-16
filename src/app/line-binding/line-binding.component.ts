1870d05fd10ffb075b2686d8d1db18fb2cc60ea7
Jamie Zhang <asxasdfghjkl@gmail.com>
2022-11-16 15:08:42 +0800

    fix binding page showing after bound

--- src/app/line-binding/line-binding.component.ts
+++ src/app/line-binding/line-binding.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AuthorityService } from '../_services/authority.service';
 import { LineBindingDialogComponent } from './line-binding-dialog/line-binding-dialog.component';
 
 @Component({
@@ -9,12 +10,16 @@ import { LineBindingDialogComponent } from './line-binding-dialog/line-binding-d
   styleUrls: ['./line-binding.component.scss'],
 })
 export class LineBindingComponent implements OnInit {
-  constructor(private router: Router, private dialog: MatDialog) {}
+  constructor(
+    private router: Router,
+    private dialog: MatDialog,
+    private auth: AuthorityService
+  ) {}
 
   ngOnInit(): void {
     const lineToken = window.location.hash.substring(1);
 
-    if (lineToken) {
+    if (!this.auth.currentUserUuid$.value && lineToken) {
       this.dialog.open(LineBindingDialogComponent, { disableClose: true });
     } else {
       this.router.navigateByUrl(`/`, { replaceUrl: true });
