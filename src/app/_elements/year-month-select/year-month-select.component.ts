e8e8113bc3569cd342a4f1016da22b77004045af
Huang Kuan Wei <33425166+xellosiris@users.noreply.github.com>
2023-02-06 23:27:53 +0800

    Feature/filter improve (#82)
    * month select
    
    * date selector component update
    
    * fix sorting on peronsal shifts page
    add personal filter
    
    * change ts version 4.7.4
    
    ---------
    
    Co-authored-by: Jamie Zhang <asxasdfghjkl@gmail.com>
    Co-authored-by: Jamie Zhang <jamie.zhang@waltonmgt.com>

--- src/app/_elements/year-month-select/year-month-select.component.ts
+++ src/app/_elements/year-month-select/year-month-select.component.ts
@@ -1,80 +1,34 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewChild
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MAT_DATE_FORMATS } from '@angular/material/core';
-import { MatCalendarView, MatDatepicker } from '@angular/material/datepicker';
-import { Moment } from 'moment';
 
 @Component({
   selector: 'app-year-month-select',
   templateUrl: './year-month-select.component.html',
-  providers: [
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: {
-        parse: {
-          dateInput: ['YYYY-MM'],
-        },
-        display: {
-          dateInput: 'YYYY-MM',
-          monthYearLabel: 'MM YYYY',
-          dateA11yLabel: 'LL',
-          monthYearA11yLabel: 'MM YYYY',
-        },
-      },
-    },
-  ],
 })
 export class YearMonthSelectComponent implements OnInit {
   constructor() {}
-
-  @ViewChild('yearMonthPicker')
-  picker: MatDatepicker<Moment>;
-
-  @Input('forwardFormControl')
-  formControl: FormControl;
+  readonly currentMonth = new Date().toJSON().substring(0, 7);
+  @Input()
+  forwardFormControl: FormControl;
 
   @Input()
   label: string;
 
-  @Input()
-  startMonth: Moment;
-
-  @Input()
-  endMonth: Moment;
-
-  private _value: string;
-  @Input()
-  get value(): string {
-    return this._value;
-  }
-  set value(newValue: string) {
-    if (this._value !== newValue) {
-      this._value = newValue;
-      this.valueChange.emit(newValue);
-    }
-  }
+  months: string[] = [];
 
   @Output()
   valueChange = new EventEmitter<string>();
 
-  ngOnInit(): void {}
-
-  setMonthAndYear(normalizedMonthAndYear: Moment) {
-    const controlValue = this.formControl?.value;
-    if (controlValue !== null) {
-      controlValue.year(normalizedMonthAndYear.year());
-      controlValue.month(normalizedMonthAndYear.month());
-      this.formControl.setValue(controlValue);
+  ngOnInit(): void {
+    const months = [];
+    for (let i = -3; i <= 1; ++i) {
+      const d = new Date();
+      d.setMonth(d.getMonth() + i);
+      months.push(d.toJSON().substring(0, 7));
     }
-    this.picker.close();
+    this.months = months;
+    if (this.forwardFormControl && !this.forwardFormControl.value) {
+      this.forwardFormControl.setValue(this.currentMonth);
+    }
   }
-
-  onViewChanged(view: MatCalendarView) {}
 }
--- src/app/opening-shifts/opening-shifts.component.ts
+++ src/app/opening-shifts/opening-shifts.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import * as moment from 'moment';
 import {
   combineLatest,
   filter,
@@ -14,7 +13,6 @@ import {
 import { Shift } from '../_interfaces/shift.interface';
 import { AuthorityService } from '../_services/authority.service';
 import { GlobalEventService } from '../_services/global-event.service';
-import { ShiftHoursService } from '../_services/shift-hours.service';
 import { ShiftsService } from '../_services/shifts.service';
 
 @Component({
@@ -23,50 +21,32 @@ import { ShiftsService } from '../_services/shifts.service';
   styleUrls: ['./opening-shifts.component.scss'],
 })
 export class OpeningShiftsComponent implements OnInit, OnDestroy {
-  yearMonthControl = new FormControl(moment());
+  yearMonthControl = new FormControl<string>('');
   shifts$!: Observable<Shift[]>;
-  today = moment();
   readonly unsubscribe$: Subject<void> = new Subject();
   constructor(
     private shiftService: ShiftsService,
-    private shiftHourService: ShiftHoursService,
     private auth: AuthorityService,
     private globalEvent: GlobalEventService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.shifts$ = combineLatest([
-      combineLatest([
-        this.globalEvent
-          .getGlobalEventById('SHIFTS_CHANGED')
-          .pipe(startWith(undefined)),
-        this.yearMonthControl.valueChanges.pipe(startWith(undefined)),
-      ]).pipe(
-        map(() => this.yearMonthControl.value),
-        filter((value) => !!value),
-        map((value) => value!.format('YYYY-MM')),
-        switchMap((yearMonth) =>
-          from(
-            this.shiftService.getOpeningShift(
-              yearMonth,
-              this.auth.currentUserUuid$.value!
-            )
+      this.globalEvent
+        .getGlobalEventById('SHIFTS_CHANGED')
+        .pipe(startWith(undefined)),
+      this.yearMonthControl.valueChanges.pipe(startWith(undefined)),
+    ]).pipe(
+      map(() => this.yearMonthControl.value),
+      filter((value) => !!value),
+      switchMap((yearMonth) =>
+        from(
+          this.shiftService.getOpeningShift(
+            yearMonth!,
+            this.auth.currentUserUuid$.value!
           )
         )
-      ),
-      this.shiftHourService.getShiftHours(),
-    ]).pipe(
-      map(([shifts, shiftHours]) => {
-        const hour = (shiftHourUuid: string) =>
-          shiftHours?.find((hour) => hour.uuid === shiftHourUuid)?.startTime ??
-          '00:00';
-        return shifts.sort((a, b) => {
-          if (a.date !== b.date) return a.date.localeCompare(b.date);
-          if (a.siteUuid !== b.siteUuid)
-            return a.siteUuid.localeCompare(b.siteUuid);
-          return hour(a.shiftHoursUuid).localeCompare(hour(b.shiftHoursUuid));
-        });
-      })
+      )
     );
   }
 
