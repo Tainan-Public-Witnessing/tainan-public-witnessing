cddb8f73e5e1da7815bedb6026d86b11fc4558ad
Jamie Zhang <asxasdfghjkl@gmail.com>
2023-06-07 18:03:43 +0800

    Site Shift UI adjustments
    i18n improvement
    data row/column swap
    upgrade tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
     screens: {
-      sm: '600px',
-      md: '960px',
-      lg: '1280px',
-      xl: '1920px',
+      sm: "600px",
+      md: "960px",
+      lg: "1280px",
+      xl: "1920px",
     },
     colors: {
-      primary: '#3f51b5',
-      accent: '#9e9e9e',
-      warn: '#f44336',
-      white: '#ffffff'
-    }
+      ...colors,
+      primary: "#3f51b5",
+      accent: "#9e9e9e",
+      warn: "#f44336",
+      white: "#ffffff",
+    },
   },
   plugins: [],
   important: true,
-}
+};
